349f9fe00c44041222e31faa7304a150f668ea3b
quaktuss <mathieupheron1@gmail.com>
2022-04-17 16:10:30 +0200

    ds

--- tp_fakebomb.cpp
+++ tp_fakebomb.cpp
@@ -1,5 +1,3 @@
-// définition des broches du décodeur 7 segments
-// (vous pouvez changer les numéros si vous voulez)
 #include <Keypad.h>
 
 const int bit_A = 2;
@@ -7,27 +5,28 @@ const int bit_B = 3;
 const int bit_C = 4;
 const int bit_D = 5;
 
-// définitions des broches des transistors pour chaque afficheur
-const int alim_ten = 6; // les tens
-const int alim_unity = 7;   // les units
+const int alim_ten = 6; //  tens
+const int alim_unity = 7;   // units
 
-// definitions des broches pave
+char password[4]; // number of characters in our password
+int currentLength = 0; //defines which number we are currently writing
+int second = 12; // number second
+char entered[4];
+
 const int ROW_NUM = 4; //four rows
 const int COLUMN_NUM = 3; //three columns
-
 char keys[ROW_NUM][COLUMN_NUM] = {
         {'1', '2', '3'},
         {'4', '5', '6'},
         {'7', '8', '9'},
         {'*', '0', '#'}
 };
-byte pin_rows[ROW_NUM] = {15, 12, 10, 9}; //connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {7, 8, 14}; //connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {12, 11, 10, 9}; //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {8, 14, 15}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 
 void setup() {
     Serial.begin(9600);
-    // Les broches sont toutes des sorties
     pinMode(bit_A, OUTPUT);
     pinMode(bit_B, OUTPUT);
     pinMode(bit_C, OUTPUT);
@@ -35,7 +34,6 @@ void setup() {
     pinMode(alim_ten, OUTPUT);
     pinMode(alim_unity, OUTPUT);
 
-    // Les broches sont toutes mises à l'état bas
     digitalWrite(bit_A, LOW);
     digitalWrite(bit_B, LOW);
     digitalWrite(bit_C, LOW);
@@ -43,67 +41,77 @@ void setup() {
     digitalWrite(alim_ten, LOW);
     digitalWrite(alim_unity, LOW);
     pinMode(13, OUTPUT);
+    Serial.println("Press * to start !");
 }
 
-void loop() // fonction principale
-{
-    // boucle qui permet de compter de 0 à 99 (= 100 valeurs)
-    for (char i = 15; i >= 0; i--) {
-        // appel de la fonction affichage avec envoi du number à display
-        display_number(i);
-        if (i < 10) {
-            digitalWrite(13, HIGH); //le courant est envoyé sur la borne 0, le buzzer se met à sonner
+void loop() {
+    char key = keypad.getKey();
+    if (key == '*') {
+        Serial.print("\nEnter Password : ");
+        while (currentLength < 4) {
+            char key2 = keypad.getKey();
+            countdown();
+            if (key2 != NO_KEY) {
+                Serial.print(key2);
+                entered[currentLength] = key2;
+                currentLength++;
+            }
+        }
+        if (currentLength == 4) { // Password 0000 (yes yes its secure)
+            if (entered[0] == '0' && entered[1] == '0' && entered[2] == '0' && entered[3] == '0') {
+                Serial.println("\nWell play you survive");
+                exit(0);
+                tetrisTheme();
+            } else {
+                Serial.println("\nWrong Password! Press * to retry.");
+                currentLength = 0;
+            }
         }
     }
-    digitalWrite(13, LOW);
-    tetrisTheme();
-    exit(0);
 }
 
+void countdown() {
+    second--;
+    display_number(second);
+    switch (second) {
+        case 10:
+            tone(13, 3136, 200);
+            delay(500);
+            break;
+        case 0:
+            Serial.println("\nKABOOM!(Yes, you die)");
+            tetrisTheme();
+            exit(0);
+    }
+}
 
-// Function who permitt to display numbers on 2 screens
+// Function who permit displaying numbers on 2 screens
 void display_number(char number) {
-    long temps; // variable utilisée pour savoir le temps écoulé...
-    char unity = 0, ten = 0; // variable pour chaque afficheur
+    long temps; // variable used to know the elapsed time
+    char unity = 0, ten = 0; // variable for each display
 
-    if (number > 9) // si le number reçu dépasse 9
+    if (number > 9) // if the number received is more than 9
     {
-        ten = number / 10; // on récupère les tens
+        ten = number / 10; // get the tens
     }
 
-    unity = number - (ten * 10); // on récupère les unités
-    temps = millis(); // on récupère le temps courant
+    unity = number - (ten * 10); // get the units
+    temps = millis(); // get the current time
 
-    // tant qu'on a pas affiché ce digit pendant au moins 500 millisecondes
-    // permet donc de pouvoir lire le number affiché
     while ((millis() - temps) < 1000) {
-        // on affiche le number
-
-        // d'abord les tens pendant 10 ms
-
-        // le transistor de l'afficheur des tens est saturé,
-        // donc l'afficheur est allumé
+        // TENS
         digitalWrite(alim_ten, HIGH);
-        // on appel la fonction qui permet d'display le digit ten
         display(ten);
-        // l'autre transistor est bloqué et l'afficheur éteint
         digitalWrite(alim_unity, LOW);
-        delay(250);
-
-        // puis les unités pendant 10 ms
-
-        // on éteint le transistor allumé
+        delay(50);
+        // UNITY
         digitalWrite(alim_ten, LOW);
-        // on appel la fonction qui permet d'display le digit unité
         display(unity);
-        // et on allume l'autre
         digitalWrite(alim_unity, HIGH);
-        delay(250);
+        delay(50);
     }
 }
 
-// fonction écrivant sur un seul afficheur
-// on utilise le même principe que vu plus haut
 void display(char digit) {
     digitalWrite(bit_A, LOW);
     digitalWrite(bit_B, LOW);
@@ -128,6 +136,7 @@ void display(char digit) {
     }
 }
 
+// TETRIS SONG
 void tetrisTheme() {
     tone(13, 2637, 200);
     delay(400);
